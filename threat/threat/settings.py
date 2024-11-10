2de54c606a5e9a3dfcc133e5d595022898608b3d
Aadit Chadda <aaditkobe268@gmail.com>
2024-11-10 01:07:29 -0500

    reverting the database back to sqlite3

--- threat/threat/settings.py
+++ threat/threat/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,14 +78,10 @@ WSGI_APPLICATION = 'threat.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ThreatLens',
-        'USER': 'root',
-        'PASSWORD': 'Kobe-2424',
-        'HOST': 'localhost',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
